d7aa1de244f116ce9a71cef0ccb8435fc2f9deaf
Babanin_dv <loki99928@yandex.ru>
2019-09-10 10:03:30 +0700

    start

---
+++ test/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    merchantPublicKey: '2tbp1WQvsgQeziGY9vTLe9vDZNg7tmCymb4Lh6STQokqKrpCC6qrUUKEDZAJ11HeiD1M7KQXpGaBA82Hnv7KruscTkCkQR9BUwrbVbGZi1ThnDycjU1W3DzWhpezp',
+    merchantSecretKey: 'eyJ2ZXJzaW9uIjoicmVzdF92MyIsImRhdGEiOnsibWVyY2hhbnRfaWQiOjI3MDMwNSwiYXBpX3VzZXJfaWQiOjg3MTIxODcwLCJzZWNyZXQiOiJjMTM2ZTY5MTg1NmYxZmRkYzYxODM5YTU2Y2I0MDdkMzdiZWViODVmN2M1MDcwNjdhYzNkNjdhODQ0ZTcyZDNlIn19',
+
+    billIdForGetRefundInfoTest: 'test_refund',
+    billRefundIdForGetRefundInfoTest: '1520425084283'
+};
